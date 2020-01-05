a0755e8ba1e64caa48b88a2f5a9c72a4ef60f4ad
Ghiath2017 <g.alshamaa@hotmail.com>
2020-01-05 16:11:54 +0100

    Created a routers for projects and orders

---
+++ Mean.js/RestApi by Node.js/api/routes/products.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const router = express.router();
+
+router.get('/', (res, req, next) => {
+    res.status(200).json({
+        message: 'Handling Get Requests to /products'
+    });
+});
+
+router.post('/', (res, req, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product
+    })
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'You can discovered the special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
