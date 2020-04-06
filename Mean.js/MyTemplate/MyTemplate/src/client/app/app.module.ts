6d277040e806b86984d958b478c8255c167e796f
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-06 17:13:21 +0200

    import the auth module in app.module & rename register

--- Mean.js/MyTemplate/MyTemplate/src/client/app/app.module.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/app.module.ts
@@ -13,11 +13,17 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 
+/* Auth Module */
+
+import { AuthModule } from "./auth/auth.module";
+
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    /* Another Modules */
+    AuthModule,
     /* Material Section*/
     BrowserAnimationsModule,
     MatToolbarModule,
