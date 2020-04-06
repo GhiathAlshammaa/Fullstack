2f52a0ef17a195b0d7bc7bf0c05b541b0fcc6d8b
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-06 19:30:44 +0200

    improved desgin for entire app

--- Mean.js/MyTemplate/MyTemplate/src/client/app/auth/auth.module.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/auth/auth.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './componets/register/register.component';
-import { LoginComponent } from './componets/login/login.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RegisterComponent } from "./componets/register/register.component";
+import { LoginComponent } from "./componets/login/login.component";
 
-
+/* Material */
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    /* Material */
+    MatInputModule,
+    MatFormFieldModule,
+    MatCardModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
