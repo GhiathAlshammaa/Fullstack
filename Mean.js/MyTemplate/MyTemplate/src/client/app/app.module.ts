c394a16cf081613cd8a0c55b223d2dedafac4a93
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-06 09:43:24 +0200

    added a link to navBar

--- Mean.js/MyTemplate/MyTemplate/src/client/app/app.module.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+
+/* Components Section*/
 import { NavComponent } from "./components/nav/nav.component";
+import { HomeComponent } from "./components/home/home.component";
 
 /* Material Section*/
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
-import { HomeComponent } from './components/home/home.component';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent],
@@ -18,6 +21,7 @@ import { HomeComponent } from './components/home/home.component';
     /* Material Section*/
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
