33d26f1dbe4e50ae231c032d503a8daba9f8fc41
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-07 17:41:43 +0200

    add validation functions for password and email to register form

--- Mean.js/MyTemplate/MyTemplate/src/client/app/auth/componets/register/register.component.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/auth/componets/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "register",
@@ -11,18 +11,37 @@ export class RegisterComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) {
     this.form = formBuilder.group({
-      firstName: "",
+      firstName: ["", Validators.required],
       lastName: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-    });
+      email: ["", [Validators.required, emailValid()]],
+      password: ["", Validators.required],
+      confirmPassword: ["", Validators.required]
+    }, {validator: matchingFields('password', 'confirmPassword')});
   }
 
   ngOnInit(): void {}
 
   onSubmit() {
-    console.log(this.form.value);
+    console.log(this.form.errors);
     return false;
+  }
+
+  isValid(control) {
+    return this.form.controls[control].invalid && this.form.controls[control].touched;
+  }
+}
+
+function matchingFields(field1, field2) {
+  return form => {
+    if (form.controls[field1].value !== form.controls[field2].value){
+      return { mismatchedFields: true }
+    }
+  }
+}
+
+function emailValid() {
+  return control => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(control.value) ? null : { invalidEmail: true };
   }
 }
