a7152de124bb1fbb4e92f58abcc4f257942cd6c5
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-07 08:37:27 +0200

    imporve Register's style

--- Mean.js/MyTemplate/MyTemplate/src/client/app/auth/auth.module.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 /* Form */
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
@@ -20,6 +21,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
     MatFormFieldModule,
     MatCardModule,
     MatButtonModule,
+    MatIconModule,
     /* Form */
     FormsModule,
     ReactiveFormsModule,
