bbfa5f6f0c3539b64ca5d027af4bf5cde520569d
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-12 15:54:58 +0200

    improve Authentication code and added login

--- Mean.js/MyTemplate/MyTemplate/src/client/app/auth/auth.service.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/auth/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   BASE_URL = 'http://localhost:3000/auth';
   NAME_KEY = 'name';
   TOKEN_KEY = 'token';
-  constructor(private http:HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
   get name() {
     return localStorage.getItem(this.NAME_KEY);
   }
@@ -18,25 +18,36 @@ export class AuthService {
   get isAuthenticated() {
     return !!localStorage.getItem(this.TOKEN_KEY);
   }
+
+  login(loginData) {
+    const url = `${this.BASE_URL}/login`;
+    this.http.post<any>(url, loginData).subscribe(res => {
+      this.authenticate(res);
+    });
+  }
   register(user){
 
     const url = `${this.BASE_URL}/register`;
     delete user.confirmPassword;
     this.http.post<any>(url, user).subscribe(res => {
-        
-        var authResponse = res;
-
-        if(!authResponse)
-          return
-
-         localStorage.setItem(this.TOKEN_KEY, authResponse.token);
-         localStorage.setItem(this.NAME_KEY, authResponse.firstName);
-         this.router.navigate(['/register']);
+        this.authenticate(res, '/login');
 
     });
   }
   logout() {
     localStorage.removeItem(this.NAME_KEY);
     localStorage.removeItem(this.TOKEN_KEY);
+  }
+
+  authenticate(res, url = null) {
+    const authResponse = res;
+
+    if (!authResponse) {
+      return;
+    }
+
+    localStorage.setItem(this.TOKEN_KEY, authResponse.token);
+    localStorage.setItem(this.NAME_KEY, authResponse.firstName);
+    url === null ? this.router.navigate(['/login']) : this.router.navigate([`${url}`]);
   }
 }
--- Mean.js/MyTemplate/MyTemplate/src/server/index.js
+++ Mean.js/MyTemplate/MyTemplate/src/server/index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 
 /* Data Storage Section */
-var users = [];
+var users = [{firstName: 'a', email: 'a', password: 'a', id: 0}];
 
 /* CORS Section */
 app.use(bodyParser.json());
@@ -20,6 +20,20 @@ var auth = express.Router();
 
 
 /* REST Api logic */
+// Authorization (LogIn)
+auth.post('/login', (req, res) => {
+    var user = users.find(user => user.email == req.body.email);
+    console.log(req.body);
+    if(!user)  
+        sendAuthError(res);
+
+    if(user.password == req.body.password )
+        sendToken(user, res);
+    else
+        sendAuthError(res);
+
+})
+// Authentication (Register)
 auth.post('/register', (req, res) => {
     // bring the index of user
     var index = users.push(req.body) -1; // because "user.push" return index and the Array begins from Zero as well
@@ -27,10 +41,18 @@ auth.post('/register', (req, res) => {
     var user = users[index];
     user.id = index;
 
+    sendToken(user, res);
+})
+
+function sendToken (user, res){
     var token = jwt.sign(user, '123');
     // the token should to put it in an object before send it.
     res.json({firstName: user.firstName ,token : token});
-})
+}
+
+function sendAuthError(res) {
+    return res.json({success: false, message: 'Email or Password incoreect'});;
+}
 
 /* Binding the logic with router */
 app.use('/auth', auth);
