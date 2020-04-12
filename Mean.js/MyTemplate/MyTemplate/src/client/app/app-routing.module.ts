9b96ad39f37a3dfee72fa634056c29c3bff20f23
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-12 14:41:21 +0200

    Added login link to router module

--- Mean.js/MyTemplate/MyTemplate/src/client/app/app-routing.module.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule, Component } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./components/home/home.component";
 import { RegisterComponent } from "./auth/componets/register/register.component";
+import { LoginComponent } from './auth/componets/login/login.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
 ];
 
 @NgModule({
