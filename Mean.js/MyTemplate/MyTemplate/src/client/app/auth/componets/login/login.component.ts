2f2bdf24653a5983bb02cf5a1b8fecb8e9f53e44
Ghiath2017 <g.alshamaa@hotmail.com>
2020-04-12 15:55:34 +0200

    added code and binded with the web service

--- Mean.js/MyTemplate/MyTemplate/src/client/app/auth/componets/login/login.component.ts
+++ Mean.js/MyTemplate/MyTemplate/src/client/app/auth/componets/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
+  constructor(private auth: AuthService) {}
 
-  constructor() { }
+  loginData = {
+    email: "",
+    password:""
+  };
 
-  ngOnInit(): void {
+ngOnInit(): void {
   }
 
+  login() {
+    this.auth.login(this.loginData);
+  }
 }
